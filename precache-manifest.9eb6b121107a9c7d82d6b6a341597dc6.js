42d3c84d280b2de1a9d661e1ceb51d25e99d7ade
Egzon Arifi <egzon.arifi@jaywalker-digital.ch>
2019-03-10 10:49:59 +0100

    Updates

---
+++ precache-manifest.9eb6b121107a9c7d82d6b6a341597dc6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d44055d6a47df79de63",
+    "url": "/EgzonArifi/egzonarifi.github.io/static/js/main.f2a2ca23.chunk.js"
+  },
+  {
+    "revision": "e55a5820db2a5255544b",
+    "url": "/EgzonArifi/egzonarifi.github.io/static/js/runtime~main.a016d43c.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/EgzonArifi/egzonarifi.github.io/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "38b1f90412d5006656bc0a009688f43f",
+    "url": "/EgzonArifi/egzonarifi.github.io/index.html"
+  }
+];
